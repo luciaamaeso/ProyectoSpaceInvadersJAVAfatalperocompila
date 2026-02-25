d63de9b18d4f0099fb6d3a8c1bad981a7c25705c
naroa <mqnaroa@gmail.com>
2026-02-25 11:16:50 +0100

    shot actualizado

--- SpaceInvaders/src/model/Shot.java
+++ SpaceInvaders/src/model/Shot.java
@@ -1,17 +1,40 @@
 package model;
 
-public abstract class Shot {
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Observable;
+
+public abstract class Shot extends Observable {
 
 	private int posX;
 	private int posY;
 	
+	private Timer timer;
+	
 	protected Shot(int x, int y) {		// por ser clase abstracta
 		this.posX=x;	
 		this.posY=y+2;	// posY es la posY de la nave + 2 (empieza 2px más arriba) 
+		moveEvery50ms();
+
 	}
 		
-	public void move() {
-		this.posY++;
+	private void move() {
+		this.posY--;
+		setChanged();
+		notifyObservers();
+		
 	}
+	
+	private void moveEvery50ms(){
+		timer = new Timer();
+		
+		timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                move();
+            }
+        }, 0, 50);	
+}
+	
 	
 }
