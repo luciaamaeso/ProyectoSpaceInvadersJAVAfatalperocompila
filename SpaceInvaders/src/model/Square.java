04357aa709f26bb5ea6e0b4220dabad633b3668f
naroa <mqnaroa@gmail.com>
2026-03-02 16:53:02 +0100

    square actualizada. faltan cosillas

--- SpaceInvaders/src/model/Square.java
+++ SpaceInvaders/src/model/Square.java
@@ -1,76 +1,40 @@
 package model;
-import java.util.ArrayList;
 
 public class Square {
 	private int posX;
 	private int posY;
-	private Alien alien=null;
-	private Shot shot=null;
-	private SpaceCraft spaceCraft=null;
-	
+	private SpaceCraft player=null;
 	
 	public Square(int x, int y) {
 		this.posX=x;	
 		this.posY=y;
 	}
 	
-	public void addShot(Shot shotType) {
-		if (shot!=null) {
-			shot=shotType;	// Ahora solo pixel, en un futuro otros tipos
-		}
-	}
-	
-	public void deleteShot() {
-		shot=null;
-	}
-	
-	
-	public void addAlien() {
-		if (alien!=null) {
-			alien= new Alien(posX, posY);
-		}
-	}
-	
-	public void deleteAlien() {
-		if (shot!=null) {		// Si hay un disparo en la casilla, se elimina al alien
-			alien=null;
-		}
-	}
-	
-	public void addSpaceCraft(SpaceCraft sc) {
-		if (spaceCraft!=null) {
-			this.spaceCraft=sc;
-		}
-	}
-	
-	public void deleteSpaceCraft() {
-		spaceCraft=null;
-	}
-	
 	public int getX() { return this.posX;}
 	public int getY() { return this.posY;}
 	
-	public void move(int x, int y) { this.posX = x; this.posY = y; }
-	
-	public boolean alienSquare() {
-		if(this.alien != null) { 
-			return true;
-		} 
-		return false;
+	public void addPlayer() {
+		player= new Player(this.posX, this.posY, "Color");
 	}
 	
-	public boolean shotSquare() {
-		if(this.shot != null) { 
-			return true;
-		} 
-		return false;
+	public void deletePlayer() {
+		this.player=null;
 	}
 	
-	public boolean spaceCraftSquare() {
-		if(this.spaceCraft != null) { 
-			return true;
-		} 
-		return false;
+	public boolean isPlayerInSquare() {
+		return this.player==null;
+	}
+	
+	public void playerShoots() {
+		this.player.shoot();
+	}
+	
+	public void movePlayerRight() {	
+		this.player.moveRight();
+	}
+	
+	public void movePlayerLeft() {	
+		this.player.moveLeft();
 	}
 	
 }
