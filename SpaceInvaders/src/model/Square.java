ff2a0cc76593c7063be85d227046f1837dc85f56
naroa <mqnaroa@gmail.com>
2026-03-02 21:33:52 +0100

    añadidos metodos que tienen que ver con la clase alien

--- SpaceInvaders/src/model/Square.java
+++ SpaceInvaders/src/model/Square.java
@@ -4,6 +4,7 @@ public class Square {
 	private int posX;
 	private int posY;
 	private SpaceCraft player=null;
+	private SpaceCraft alien=null;
 	
 	public Square(int x, int y) {
 		this.posX=x;	
@@ -12,10 +13,11 @@ public class Square {
 	
 	public int getX() { return this.posX;}
 	public int getY() { return this.posY;}
-	public void move(int x, int y) { this.posX = x; this.posY = y;}
 	
 	public void addPlayer() {
-		player= new Player(this.posX, this.posY, "Color");
+		if (this.player==null) {
+			player= new Player(this.posX, this.posY, "Color");
+		}
 	}
 	
 	public void deletePlayer() {
@@ -38,6 +40,17 @@ public class Square {
 		this.player.moveLeft();
 	}
 
+	public void addAlien() {
+		if (alien==null) {
+			this.alien= new Alien(posX, posY);
+		}
+	}
 	
+	public void deleteAlien() {
+		this.alien=null;
+	}
 	
+	public boolean isAlienInSquare() {
+		return this.alien==null;
+	}
 }
